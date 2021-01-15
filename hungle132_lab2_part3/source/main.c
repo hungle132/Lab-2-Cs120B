87870729b850981f7b061618ccb5df47f7800326
Ubuntu <hungiscoolguy@yahoo.com>
2021-01-15 01:15:50 -0800

    updated

--- hungle132_lab2_part3/source/main.c
+++ hungle132_lab2_part3/source/main.c
@@ -13,11 +13,52 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
 
+
+		  /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's  pins as inputs
+	DDRC = 0xFF; PORTC = 0x00;
+
+	unsigned char tmpA = 0x00;
+	unsigned char tmpA1 = 0x00;
+	unsigned char tmpA2 = 0x00;
+	unsigned char tmpA3 = 0x00;
+	unsigned char cntavail = 0x00;
     /* Insert your solution below */
     while (1) {
+	cntavail = 0x04;
+	val = 0x00;
+	tmpA = PINA & 0x01;//pin 0
+	tmpA1 = PINA & 0x02; //pin 1
+	tmpA2 = PINA & 0x04; //pin 2
+	tmpA3 = PINA & 0x08;// pin 3
 
+	if(tmpA == 1){
+		val++;
+	}
+
+	if(tmpA1 == 2){
+		
+		val++;
+	}
+	if(tmpA2 == 4){
+	
+		val++;
+	}
+	if(tmpA3 == 8){
+		
+		val++;
+	}
+	    cntavail = cntavail-val;
+	    
+	if(cntavail == 0){
+	PORTC = 0x80;
+	}
+	else{
+	PORTC = cntavail;
+	}
     }
-    return 1;
+    return 0;
+	
+		
 }
